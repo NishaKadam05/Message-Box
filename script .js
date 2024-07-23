8beadcab38e5b89b8366d02523c8c469e76969fb
Nisha Anandrao Kadam <142510987+NishaKadam05@users.noreply.github.com>
2024-07-23 18:05:17 +0530

    Add files via upload

---
+++ script .js
@@ -0,0 +1,74 @@
+function Accept(){
+    const ques=document.getElementById("question").value;
+    if(ques===""){
+        alert("Please enter a question");
+    }
+    else if(ques==="What song would make the best theme music for you?"){
+        alert("Accepted");
+    }
+    else if(ques==="What is the weirdest food combination you enjoy?"){
+        alert("Accepted");
+    }
+    else if(ques==="What is your idea of the perfect day?"){
+        alert("Accepted");
+    }
+    else if(ques==="If you could have free meals for life at one fast food chain, which one would you choose"){
+        alert("Accepted");
+    }
+    else if(ques==="When was the last time you laughed so hard that you cried?"){
+        alert("Accepted");
+    }
+    else if(ques==="What is your nickname?"){
+        alert("Accepted");
+    }
+    else if(ques==="Where is your happy place?"){
+        alert("Accepted");
+    }
+    else if(ques=="Which family member are you closest to?"){
+        alert("Accepted");
+    }
+    else if(ques=="What is one goal you have for the near future?"){
+        alert("Accepted");
+    }
+    else if(ques=="What is the best meal you have ever eaten?"){
+        alert("Accepted");
+    }
+        
+}
+
+function Decline(){
+    const ques=document.getElementById("question").value;
+    if(ques===""){
+        alert("Please enter a question");
+    }
+    else if(ques!=="What song would make the best theme music for you?"){
+        alert("Declined");
+    }
+    else if(ques!=="What is the weirdest food combination you enjoy?"){
+        alert("Declined");
+    }
+    else if(ques!=="What is your idea of the perfect day?"){
+        alert("Declined");
+    }
+    else if(ques!=="If you could have free meals for life at one fast food chain, which one would you choose"){
+        alert("Declined");
+    }
+    else if(ques!=="When was the last time you laughed so hard that you cried?"){
+        alert("Declined");
+    }
+    else if(ques!=="What is your nickname?"){
+        alert("Declined");
+    }
+    else if(ques!=="Where is your happy place?"){
+        alert("Declined");
+    }
+    else if(ques!=="Which family member are you closest to?"){
+        alert("Declined");
+    }
+    else if(ques!=="What is one goal you have for the near future?"){
+        alert("Declined");
+    }
+    else if(ques!=="What is the best meal you have ever eaten?"){
+        alert("Declined");
+    }
+}
